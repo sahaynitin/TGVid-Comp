f2ca4cb158d3c6a695307e302b7b600ef9e47108
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-09 09:51:46 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -6,8 +6,6 @@ from datetime import datetime
 START_TIME = datetime.now()
 
 async def up(event):
-    if not event.is_private:
-        return
     stt = dt.now()
     ed = dt.now()
     v = ts(int((ed - uptime).seconds) * 1000)
@@ -31,18 +29,13 @@ async def zylern(event):
 
 /start - __Check Bot is Working Or Not__
 /help - __Get Detailed Help__
-/setcode - __Set Custom FFMPEG Code__
-/getcode - __Print Current FFMPEG Code__
 /logs - __Get Bot Logs__
 /ping - __Check Ping__
-/sysinfo - __Get System Info__
 /leech - __Leech Links And Compress Video__
 /renew - __Clear Cached Downloads__
 /clear - __Clear Queued Files__
 /showthumb - __Show Current Thumbnail__
 /speed - __Do A SpeedTest__
-/eval - __Execute An Argument__
-/bash - __Run Bash Commands__
 /cmds - __List Available Commands__
 """
     )
