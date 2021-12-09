e6905c49fc3f4240453aa9392d1dcaaa5735048d
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-09 10:12:42 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -14,34 +14,3 @@ async def up(event):
     await event.reply(v + "\n" + p)
 
 
-async def start(event):
-    await event.reply(
-        f"**Send me the video which you want to compress.**\n**Uptime: {str(datetime.now() - START_TIME).split('.')[0]}**",
-        buttons=[
-            [Button.inline("HELP", data="help")],
-        ],
-    )
-
-async def zylern(event):
-    await event.reply(
-        f"""
-**Available Commands 🤖**
-
-/start - __Check Bot is Working Or Not__
-/help - __Get Detailed Help__
-/logs - __Get Bot Logs__
-/ping - __Check Ping__
-/leech - __Leech Links And Compress Video__
-/renew - __Clear Cached Downloads__
-/clear - __Clear Queued Files__
-/showthumb - __Show Current Thumbnail__
-/speed - __Do A SpeedTest__
-/cmds - __List Available Commands__
-"""
-    )
-
-
-async def help(event):
-    await event.edit(
-        f"""**To check current ffmpeg code you can use** /getcode\n\n**You can change your ffmpeg code by executing following command.**\n\n`/setcode -preset faster -c:v libx265 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1`\n\n**To set custom thumbnail send me the image.**\n\n**Do /cmds For More**"""
-    )
