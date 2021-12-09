83413e28a773127089781e059ac65c84d2ad411a
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-09 10:26:22 +0530

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -1,8 +1,4 @@
 import os
-if bool(os.environ.get("WEBHOOK", False)):
-    from config import Config
-else:
-    from config import Config
 from script import Script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
