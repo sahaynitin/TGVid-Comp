53ebaad32c95ef090acf4e927516345670983b40
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-09 10:42:25 +0530

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -2,6 +2,17 @@ import os
 from script import Script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from .worker import *
+from datetime import datetime
+
+START_TIME = datetime.now()
+async def up(event):
+    stt = dt.now()
+    ed = dt.now()
+    v = ts(int((ed - uptime).seconds) * 1000)
+    ms = (ed - stt).microseconds / 1000
+    p = f"Ping = {ms}ms"
+    await event.reply(v + "\n" + p)
 
 
 @Client.on_message(filters.command(["start"]) & filters.private)
@@ -11,10 +22,3 @@ async def event(bot, update):
         disable_web_page_preview=True,
         reply_markup=Script.START_BUTTONS
     )
-async def up(event):
-    stt = dt.now()
-    ed = dt.now()
-    v = ts(int((ed - uptime).seconds) * 1000)
-    ms = (ed - stt).microseconds / 1000
-    p = f"Ping = {ms}ms"
-    await event.reply(v + "\n" + p)
