32a4cdb000897bd6d780008821dad5a022c84e0d
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-09 10:40:48 +0530

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 @Client.on_message(filters.command(["start"]) & filters.private)
-async def start(bot, update):
+async def event(bot, update):
     await update.reply_text(
         text=Script.START_TEXT.format(update.from_user.mention),
         disable_web_page_preview=True,
